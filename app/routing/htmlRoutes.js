89f9ecd00890dab71cd070eb5e733f5a809b0efe
dwc3k5 <dwc1608@gmail.com>
2017-12-01 14:06:32 -0600

    copied in files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,51 @@
+
+var path = require("path");
+
+module.exports = function(app) {
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/checkplayers", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+};
+
+
+// Requiring our custom middleware for checking if a user is logged in
+
+
+//UNCOMMENT THIS BEFORE PUSHING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/game");
+    }
+    res.sendFile(path.join(__dirname, "../public/registration.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/game");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/lobby", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/lobby.html"));
+  });
+
+    app.get("/game", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/game.html"));
+  });
+
+};
